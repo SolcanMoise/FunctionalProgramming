3792247422944bdf91a4a24091b73582916649c9
Solcan Moise <moise.solcan@paddypowerbetfair.com>
2019-08-13 22:17:25 +0300

    Composing functions

--- functions/src/Function.java
+++ functions/src/Function.java
@@ -12,19 +12,30 @@ public interface Function {
 
 class FunctionImpl {
 
-
     /**
      * Method to double an integer value
      */
-    private static Function square = arg -> arg * 2;
+    private static Function doubleVal = arg -> arg * 2;
 
     /**
      * Method to triple an integer value
      */
-    private static Function triple = arg -> arg * 3;
+    private static Function tripleVal = arg -> arg * 3;
+
+    /**
+     * Composing function
+     *
+     * @param function1 the first function
+     * @param function2 the second function
+     * @return the composing function between function1 and function2
+     */
+    static Function compose(final Function function1, final Function function2) {
+        return arg -> function1.apply(function2.apply(arg));
+    }
 
     public static void main(String[] args) {
-        System.out.println(square.apply(2));
-        System.out.println(triple.apply(3));
+        System.out.println(doubleVal.apply(2));
+        System.out.println(tripleVal.apply(3));
+        System.out.println(compose(tripleVal, doubleVal).apply(4));
     }
 }
